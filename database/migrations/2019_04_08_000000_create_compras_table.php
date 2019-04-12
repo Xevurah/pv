f77361cf88a9d288b16f123ddefac3266ae8024b
Eduardo Ali Garcia Ponce <ali_524-@hotmail.com>
2019-04-11 22:21:07 -0500

    Tabla compras

--- database/migrations/2019_04_08_000000_create_compras_table.php
+++ database/migrations/2019_04_08_000000_create_compras_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVentasTable extends Migration
+class CreateComprasTable extends Migration
 {
 
     public function up()
